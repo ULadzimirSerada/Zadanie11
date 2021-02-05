02117000449e6926c11240498b3f2d9421d85949
UladzimirSerada <medavikiby@gmail.com>
2021-02-05 18:48:21 +0300

    (z11)

---
+++ Zadanie11.java
@@ -0,0 +1,15 @@
+/*Задание 11
+Имеется целое число, определить является ли это число простым, т.е.
+делится без остатка только на 1 и себя.*/
+        public class Zadanie11 {
+    public static void main(String[] args) {
+        int num; //что то не то-не пойму***
+        for (int n = 2; n < num; n++) {
+           if (num%n == 0){
+               System.out.println("sumНе простое число");
+           }
+        }
+
+        System.out.println("Простое число");
+    }
+}
